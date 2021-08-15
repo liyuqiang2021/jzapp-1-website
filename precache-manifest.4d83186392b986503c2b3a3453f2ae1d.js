e20f5f2f84f46bf5f239eca2026af1299aed8fa4
john <liyuqiang2021@outlook.com>
2021-08-15 22:43:12 +0800

    deploy

---
+++ precache-manifest.4d83186392b986503c2b3a3453f2ae1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd21ca3b92438337abc",
+    "url": "/jzapp-1-website/css/app.1508b7fe.css"
+  },
+  {
+    "revision": "f0742ee4b1ed36d53add2a902bda3ee9",
+    "url": "/jzapp-1-website/index.html"
+  },
+  {
+    "revision": "8fd21ca3b92438337abc",
+    "url": "/jzapp-1-website/js/app.7fbbbc70.js"
+  },
+  {
+    "revision": "13f2ad3b691842ebf2e6",
+    "url": "/jzapp-1-website/js/chunk-vendors.0d230586.js"
+  },
+  {
+    "revision": "2990a14f5f048230ceea29a20bfe89d6",
+    "url": "/jzapp-1-website/manifest.json"
+  },
+  {
+    "revision": "5818bd5b5e5da6c3da626996bb0c6c4c",
+    "url": "/jzapp-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jzapp-1-website/robots.txt"
+  }
+]);
